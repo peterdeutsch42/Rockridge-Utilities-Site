f3f5b2223f1a60f5f58c9811c222048ee6e87ea3
Peter Deutsch <me@peterdeutsch.ca>
2013-03-12 23:23:18 -0700

    Added more features for timings

--- index.php
+++ index.php
@@ -7,15 +7,6 @@
 This is a test site
 </p>
 <div align="center">
-<p>Dummy Table</p>
-<table>
-<tr>
-<td>1</td>
-<td>2</td>
-<td>3</td>
-<td>4</td>
-</tr>
-</table>
 <p>PHP dohicky</p>
 <?php
 mysql_connect('localhost','root','');
@@ -24,9 +15,21 @@ $date = date("Y-m-d");
 echo $date;
 $result = mysql_query("SELECT * FROM `blockrotation` WHERE `Date` = '$date'");
 $row = mysql_fetch_array($result);
+echo "<br />";
+$day = $row['Day'];
+echo "Day $day";
+echo "<br />";
 echo $row['Rotation'];
 $xblock = $row['xblock'];
-echo $xblock;
+echo "<br />";
+if ($xblock = '0')
+	{
+	echo "There is no X-Block today";
+	}
+	else
+	{
+	echo "There is X-Block today";
+	}
 
 ?>
 </div>
